3571cebeb1201ef776f9d235e0f743728b0edf65
unknown <rafi.dhega@gmail.com>
2020-11-25 22:36:23 +0700

    project pertama

---
+++ app/src/main/java/com/example/android/teskotlin/WordRepository.kt
@@ -0,0 +1,23 @@
+
+package com.example.android.teskotlin
+
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Abstracted Repository as promoted by the Architecture Guide.
+ * https://developer.android.com/topic/libraries/architecture/guide.html
+ */
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+    suspend fun delete(word: Word) {
+        wordDao.deleteAll()
+    }
+}
